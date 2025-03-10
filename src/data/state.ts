8e317f0f670e3a1b1d0c556d768b95beca623925
Alexander Rindo <alexander.rindo@dn.se>
2025-03-10 16:13:19 +0900

    Changed offset factor

--- src/data/state.ts
+++ src/data/state.ts
@@ -18,37 +18,37 @@ export const playerStates = [{
 }, {
     label: 'idleBack',
     frames: 12,
-    offset: 48,
+    offset: 1,
     animationDelay: 7
 }, {
     label: 'runForward',
     frames: 8,
-    offset: 48 * 2,
+    offset: 2,
     animationDelay: 6
 }, {
     label: 'runBack',
     frames: 8,
-    offset: 48 * 3,
+    offset: 3,
     animationDelay: 6
 }, {
     label: 'jumpForward',
     frames: 8,
-    offset: 48 * 4,
+    offset: 4,
     animationDelay: 6
 },  {
     label: 'jumpBackward',
     frames: 8,
-    offset: 48 * 4,
+    offset: 5,
     animationDelay: 6
 }, {
     label: 'attackForward',
-    frames: 8,
-    offset: 48 * 4,
+    frames: 6,
+    offset: 6,
     animationDelay: 6
 },  {
     label: 'attackBack',
-    frames: 8,
-    offset: 48 * 4,
+    frames: 6,
+    offset: 7,
     animationDelay: 6
 }]
 
